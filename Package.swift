1e6d016a7ec8e0eea1534fd3b5c6d55db9fdaf53
Hale Xie <hale.xie@helplightning.com>
2025-09-11 17:54:54 +0800

    SPM - Add Package.swift for SPM support

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UIActivityIndicator-for-SDWebImage",
+    platforms: [
+        .iOS(.v16),
+        .visionOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "UIActivityIndicator-for-SDWebImage",
+            targets: ["UIActivityIndicator-for-SDWebImage"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "git@github.com:SDWebImage/SDWebImage.git", from: "5.0.0")
+    ],
+    targets: [
+        .target(
+            name: "UIActivityIndicator-for-SDWebImage",
+            dependencies: ["SDWebImage"],
+            path: ".",
+            sources: [
+                "UIImageView+UIActivityIndicatorForSDWebImage.m"
+            ],
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath(".")
+            ]
+        ),
+    ]
+)
